5b11bdcc076402283ca932a62f296959b3068d9e
Hanif Rais <hanifrais17@gmail.com>
2025-08-17 22:13:19 +0100

    colorscheme options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.nu = true 
 vim.opt.relativenumber = true 
 vim.opt.cursorline = true 
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.smartindent = true
 vim.opt.expandtab = true 
