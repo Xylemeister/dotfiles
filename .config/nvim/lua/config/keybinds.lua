683402e9e97534731d10ad5d35ccf1de91448138
Hanif Rais <hanifrais17@gmail.com>
2025-08-19 18:57:37 +0100

    nice search

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -4,3 +4,10 @@ vim.keymap.set("n", "<leader>e", ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>y', '"+yy', { noremap = true, silent = true })
 vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<CR>", function()
+  if vim.v.hlsearch == 1 then
+    vim.cmd("nohlsearch")
+    return ""
+  end
+  return "<CR>"
+end, { expr = true, silent = true })
