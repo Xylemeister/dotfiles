81299387d7e69b6414324252d386ff2b9613d3a6
Hanif Rais <hanifrais17@gmail.com>
2025-08-26 23:23:30 +0100

    minor changes

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -7,8 +7,9 @@ return {
     config = function () 
       require("oil").setup({
         view_options = {
-          show_hidden = true, 
+          show_hidden = true,
         },
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
       })
     end,
   }
